cac66929afa857f6893e9b0cb78d30c039615bd8
hasanihardik <hasanihardik@gmail.com>
2025-08-02 18:28:51 +0530

    form changes- 5

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,14 +21,14 @@ export const extractUUIDFromString = (url: string) => {
 
 export const pusherServer = new PusherServer({
   appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KE as string,
-  secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRE as string,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
+  secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRET as string,
   cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
   useTLS: true,
 })
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KE as string,
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
   {
     cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
   }
